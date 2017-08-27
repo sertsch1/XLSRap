6f381708aa0d659a95fa5f4da69d9a7d05399f8d
Sergei <sergei@sphinx.li>
2017-08-27 04:27:10 +0200

    Add XLSModel

---
+++ src/ui/XLSModel.cpp
@@ -0,0 +1,11 @@
+#include <XLSModel.h>
+
+XLSModel::XLSModel(const QStringList &data, QObject *parent) : QAbstractItemModel(parent) {
+
+}
+
+
+
+XLSModel::~XLSModel()
+{
+}
---
+++ src/ui/XLSModel.h
@@ -0,0 +1,15 @@
+#ifndef XLSMODEL_H
+#define XLSMODEL_H
+
+#include <QtCore\qdebug.h>
+#include <QtCore\qstringlist.h>
+
+class XLSModel {
+	XLSModel(const QStringList &data, QObject *parent);
+	~XLSModel();
+
+public:
+
+};
+
+#endif // !XLSMODEL_H
