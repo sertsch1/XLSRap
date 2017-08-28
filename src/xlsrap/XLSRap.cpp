cdd1ec244691d45ccdc42254eb55a441a8753e27
Sergei <sergei@sphinx.li>
2017-08-28 02:48:40 +0200

    Add functionality to load .xlsx files.

--- src/xlsrap/XLSRap.cpp
+++ src/xlsrap/XLSRap.cpp
@@ -1,7 +1,43 @@
 #include <xlsrap/XLSRap.hpp>
+#include <iterator>
 
 XLSRAP_BEGIN_NS
 
+void XLSRap::LoadTable(std::string PathToFile) {
+	//Construct path
+	auto PathToTable = xlnt::path(PathToFile);
+	Table.load(PathToTable);
+}
 
+void XLSRap::LoadSheet(int id) {
+	Sheet = Table.sheet_by_index(id);
+}
+
+void XLSRap::LoadSheet()
+{
+	Sheet = Table.sheet_by_index(0);
+}
+
+void XLSRap::LoadSheetIntoTable(xlnt::worksheet CurrentSheet)
+{
+	auto ColumnRange = CurrentSheet.cend();
+	auto RowRange = CurrentSheet.cend();
+
+}
+
+int XLSRap::GetColumnCount(xlnt::worksheet CurrentSheet) {
+	//xlnt::range Range = CurrentSheet.columns();
+	auto LastColumn = CurrentSheet.highest_column();
+	return LastColumn.index;
+	//auto Iterator = Range.cend();
+	//int count = 0;
+	
+}
+
+int XLSRap::GetRowCount(xlnt::worksheet CurrentSheet)
+{
+	auto LastRow = CurrentSheet.highest_row();
+	return LastRow;
+}
 
 XLSRAP_END_NS
--- src/xlsrap/include/xlsrap/XLSRap.hpp
+++ src/xlsrap/include/xlsrap/XLSRap.hpp
@@ -1,9 +1,26 @@
 #include <xlsrap/Config.hpp>
+#include <xlnt\xlnt.hpp>
+#include <string>
+
 
 XLSRAP_BEGIN_NS
 
 class XLSRap {
-	
+public:
+
+	xlnt::workbook Table;
+	xlnt::worksheet Sheet;
+
+	void LoadTable(std::string PathToFile);
+
+	void LoadSheet(int id);
+
+	void LoadSheet();
+
+	void LoadSheetIntoTable(xlnt::worksheet CurrentSheet);
+
+	int GetColumnCount(xlnt::worksheet CurrentSheet);
+	int GetRowCount(xlnt::worksheet CurrentSheet);
 };
 
 XLSRAP_END_NS
