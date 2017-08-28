5c8f99af00e84fdc92c8ee851288317a87120a8b
Sergei <sergei@sphinx.li>
2017-08-28 02:47:03 +0200

    Add a basic QTableView with header to display a .xlsx file.

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,16 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
+
     ui->setupUi(this);
 
-
+	connect(ui->actionOpen_File, SIGNAL(triggered()), this, SLOT(SetFileName()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+std::string MainWindow::QStringToStdString(QString StringToConvert)
+{
+	return StringToConvert.toUtf8().constData();
+}
+
+
+void MainWindow::SetFileName() {
+	FileName = QFileDialog::getOpenFileName(this, tr("Select Spreadsheet"), QDir::currentPath(), tr("XLS Files (*.xls *.xlsx);;All Files (*.*)"));
+	if (FileName != nullptr) {
+		//Load Table and Sheet
+		ui->StatusLabel->setText(QString("Loading File..."));
+		qApp->processEvents();
+		XLSRap.LoadTable(QStringToStdString(FileName));
+		XLSRap.LoadSheet();
+		//Initalize Model and update the table
+		
+		XLSModel* CurrentModel = new XLSModel(0, XLSRap.Sheet);
+		ui->XlsTableModel->setModel(CurrentModel);
+		ui->XlsTableModel->setSortingEnabled(true);
+		ui->XlsTableModel->resizeColumnsToContents();
+		ui->StatusLabel->setTextFormat(Qt::TextFormat::PlainText);
+		ui->StatusLabel->setText(QString("Finished Loading File."));
+		ui->StatusLabel->setAlignment(Qt::AlignCenter | Qt::AlignLeft);
+		qApp->processEvents();
+
+
+	}
 }
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <qfiledialog.h>
+#include <qstring.h>
+#include <qdir.h>
+#include <xlsrap\XLSRap.hpp>
+#include <string>
+
+#include <XLSModel.h>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +22,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+	
+	xlsrap::XLSRap XLSRap;
+
+	QString FileName;
+
+
+
+public slots:
+	void SetFileName();
 
 private:
     Ui::MainWindow *ui;
+
+	std::string QStringToStdString(QString StringToConvert);
 };
 
 #endif // MAINWINDOW_H
