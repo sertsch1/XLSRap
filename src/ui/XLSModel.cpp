a0a9e2e38471e83c793981660f11278768a77768
Sergei <sergei@sphinx.li>
2017-08-28 02:48:11 +0200

    Add QAbstractTableModel subclass (XLSModel)

--- src/ui/XLSModel.cpp
+++ src/ui/XLSModel.cpp
@@ -1,11 +1,38 @@
 #include <XLSModel.h>
 
-XLSModel::XLSModel(const QStringList &data, QObject *parent) : QAbstractItemModel(parent) {
-
+XLSModel::XLSModel(QObject *Parent, xlnt::worksheet CurrentWorkSheet) : QAbstractTableModel(Parent)
+{
+	ActiveWorkSheet = CurrentWorkSheet;
 }
 
 
-
-XLSModel::~XLSModel()
+int XLSModel::rowCount(const QModelIndex & /*Parent*/) const
 {
+	auto i = ActiveWorkSheet.highest_row();
+	return i - 1;
+}
+
+int XLSModel::columnCount(const QModelIndex & /*Parent*/) const
+{
+	auto i = ActiveWorkSheet.highest_column();
+	return i.index;
+}
+
+QVariant XLSModel::data(const QModelIndex &Index, int Role) const
+{
+	if (Role == Qt::DisplayRole) {
+		xlnt::cell_reference CellRef = xlnt::cell_reference(Index.column() + 1, Index.row() + 2);
+		auto Cell = ActiveWorkSheet.cell(CellRef);
+		auto CellString = Cell.to_string();
+		QString CellContent = QString::fromStdString(CellString);
+		return CellContent;
+	}
+	return QVariant();
+}
+
+QVariant XLSModel::headerData(int Section, Qt::Orientation Orientation, int Role) const
+{
+	if (Role == Qt::DisplayRole && Orientation == Qt::Horizontal)
+		return QString::fromStdString(ActiveWorkSheet.cell(xlnt::cell_reference(Section + 1, 1)).to_string());
+	return QVariant();
 }
--- src/ui/XLSModel.h
+++ src/ui/XLSModel.h
@@ -1,15 +1,27 @@
 #ifndef XLSMODEL_H
 #define XLSMODEL_H
 
+#include <XLSItem.h>
 #include <QtCore\qdebug.h>
 #include <QtCore\qstringlist.h>
+#include <xlnt\xlnt.hpp>
 
-class XLSModel {
-	XLSModel(const QStringList &data, QObject *parent);
-	~XLSModel();
 
+class XLSModel : public QAbstractTableModel {
+	Q_OBJECT
 public:
 
+	xlnt::worksheet ActiveWorkSheet;
+
+	XLSModel(QObject *Parent, xlnt::worksheet CurrentWorkSheet);
+
+	int rowCount(const QModelIndex &Parent = QModelIndex()) const;
+
+	int columnCount(const QModelIndex &Parent = QModelIndex()) const;
+
+	QVariant data(const QModelIndex &index, int Role = Qt::DisplayRole) const;
+
+	QVariant headerData(int Section, Qt::Orientation Orientation, int Role) const;
 };
 
 #endif // !XLSMODEL_H
